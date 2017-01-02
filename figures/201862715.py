229e7640dd6b73fdaf400a47743b3b5a804987a3
Rodrigo Luger <rodluger@gmail.com>
2017-01-02 16:28:25 -0200

    updates

---
+++ figures/201862715.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+201862715.py
+------------
+
+'''
+
+import everest
+import matplotlib.pyplot as pl
+from matplotlib.ticker import MaxNLocator
+import numpy as np
+import george
+
+# Planet params
+EPIC = 201862715
+t0 = 1979.95
+period = 2.65572
+dur = 0.15
+
+# Get the Everest 2 data
+evr2 = everest.Everest(EPIC)
+evr2.mask_planet(t0, period, dur)
+evr2.compute()
+
+# Get the K2SFF data
+k2sff_time, k2sff_flux = everest.k2.pipelines.get(EPIC, 'k2sff')
+
+# Fill in missing cadences
+tol = 0.005
+if not ((len(k2sff_time) == len(evr2.time)) and (np.abs(k2sff_time[0] - evr2.time[0]) < tol) and (np.abs(k2sff_time[-1] - evr2.time[-1]) < tol)):
+  ftmp = np.zeros_like(evr2.time)
+  j = 0
+  for i, t in enumerate(evr2.time):
+    if np.abs(k2sff_time[j] - t) < tol:
+      ftmp[i] = k2sff_flux[j]
+      j += 1
+      if j == len(k2sff_time) - 1:
+        break
+  k2sff_flux = ftmp
+  k2sff_time = np.array(evr2.time)
+
+# Set up the plot
+fig = pl.figure(figsize = (13, 7))
+axk2sff = pl.subplot2grid((2, 10), (0, 0), colspan = 7, rowspan = 1)
+axk2sfff = pl.subplot2grid((2, 10), (0, 7), colspan = 3, rowspan = 1)
+axevr2 = pl.subplot2grid((2, 10), (1, 0), colspan = 7, rowspan = 1)
+axevr2f = pl.subplot2grid((2, 10), (1, 7), colspan = 3, rowspan = 1)
+
+# Plot the K2SFF light curve
+axk2sff.plot(k2sff_time, k2sff_flux, 'k.', alpha = 0.3, ms = 2)
+axk2sff.set_ylim(0.9825e6, 1.0225e6)
+axk2sff.set_xlim(1977.36, 2057.33)
+
+# Plot the Everest 2 light curve
+y = np.array(evr2.flux)
+# [HACK] Remove the missing mid-campaign data
+y[np.where((evr2.time >= 2014.98) & (evr2.time <= 2017.88))] = np.nan
+axevr2.plot(evr2.time, y, 'k.', alpha = 0.3, ms = 2)
+axevr2.set_ylim(1.00e6, 1.04e6)
+axevr2.set_xlim(1977.36, 2057.33)
+
+# Plot the whitened and folded K2SFF light curve
+_, amp, tau = evr2.kernel_params
+gp = george.GP(amp ** 2 * george.kernels.Matern32Kernel(tau ** 2))
+gp.compute(evr2.apply_mask(k2sff_time), evr2.apply_mask(evr2.fraw_err))
+med = np.nanmedian(evr2.apply_mask(k2sff_flux))
+y, _ = gp.predict(evr2.apply_mask(k2sff_flux) - med, k2sff_time)
+fwhite = (k2sff_flux - y)
+fwhite /= np.nanmedian(fwhite)
+tfold = (k2sff_time - t0 - period / 2.) % period - period / 2. 
+inds = np.where(np.abs(tfold) < 1.5 * dur)[0]
+x = tfold[inds]
+y = fwhite[inds]
+axk2sfff.plot(x, y, 'k.', alpha = 0.75, ms = 3)
+axk2sfff.set_ylim(0.98, 1.005)
+axk2sfff.margins(0, None)
+axk2sfff.yaxis.tick_right()
+axk2sfff.get_yaxis().set_major_locator(MaxNLocator(4))
+axk2sfff.get_xaxis().set_major_locator(MaxNLocator(4))
+
+# Plot the whitened and folded Everest 2 light curve
+_, amp, tau = evr2.kernel_params
+gp = george.GP(amp ** 2 * george.kernels.Matern32Kernel(tau ** 2))
+gp.compute(evr2.apply_mask(evr2.time), evr2.apply_mask(evr2.fraw_err))
+med = np.nanmedian(evr2.apply_mask(evr2.flux))
+y, _ = gp.predict(evr2.apply_mask(evr2.flux) - med, evr2.time)
+fwhite = np.array(evr2.flux)
+# [HACK] Remove the missing mid-campaign data
+fwhite[np.where((evr2.time >= 2014.98) & (evr2.time <= 2017.88))] = np.nan
+fwhite -= y
+fwhite /= np.nanmedian(fwhite)
+tfold = (evr2.time - t0 - period / 2.) % period - period / 2. 
+inds = np.where(np.abs(tfold) < 1.5 * dur)[0]
+x = tfold[inds]
+y = fwhite[inds]
+axevr2f.plot(x, y, 'k.', alpha = 0.75, ms = 3)
+axevr2f.set_ylim(0.98, 1.005)
+axevr2f.margins(0, None)
+axevr2f.yaxis.tick_right()
+axevr2f.get_yaxis().set_major_locator(MaxNLocator(4))
+axevr2f.get_xaxis().set_major_locator(MaxNLocator(4))
+
+# Labels and stuff
+axevr2.set_xlabel('Time (BJD - 2454833)', fontsize = 22)
+axevr2f.set_xlabel('Time (days)', fontsize = 22)
+axevr2.set_ylabel('EVEREST 2.0 Flux', fontsize = 22)
+axk2sff.set_ylabel('K2SFF Flux', fontsize = 22)
+axevr2.annotate('13.61 ppm', xy = (0.025, 0.96), xycoords = 'axes fraction', 
+                ha = 'left', va = 'top', color = 'r', fontsize = 14)
+axk2sff.annotate('70.32 ppm', xy = (0.025, 0.96), xycoords = 'axes fraction', 
+                 ha = 'left', va = 'top', color = 'r', fontsize = 14)              
+
+# Save
+fig.savefig('201862715.pdf', bbox_inches = 'tight')
